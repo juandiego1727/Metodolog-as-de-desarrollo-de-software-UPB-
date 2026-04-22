c256daf8da7ad518f17261b2b64bc77c91ca9518
David Astudillo <davidast07@gmail.com>
2026-04-21 20:59:05 -0500

    add utils and use

--- frontend/menu.py
+++ frontend/menu.py
@@ -1,9 +1,15 @@
 from tabulate import tabulate
 from backend.productos import leer_productos, buscar_producto
+from utils.pausa import pausa
+from utils.separador import separador
+from utils.limpiar import limpiar
 
 RUTA = "data/productos.xlsx"
 
 def mostrar_productos():
+    separador()
+    print("LISTADO DE PRODUCTOS")
+    separador()
     productos = leer_productos(RUTA)
 
     tabla = []
@@ -11,23 +17,34 @@ def mostrar_productos():
         tabla.append([p["id"], p["nombre"], p["precio"], p["cantidad"]])
 
     print(tabulate(tabla, headers=["ID", "Nombre", "Precio", "Cantidad"], tablefmt="grid"))
+    separador()
+    pausa()
 
 
 def consultar_producto():
+    separador()
     productos = leer_productos(RUTA)
     id_buscar = int(input("Ingrese ID: "))
 
     producto = buscar_producto(productos, id_buscar)
 
     if producto:
+        separador()
+        print("RESULTADO DE BÚSQUEDA")
         print(tabulate([[producto["id"], producto["nombre"], producto["precio"], producto["cantidad"]]],
               headers=["ID", "Nombre", "Precio", "Cantidad"], tablefmt="grid"))
     else:
         print("Producto no encontrado")
+    separador()
+    pausa()
 
 
 def menu():
+    limpiar()
     while True:
+        separador()
+        print("MENÚ DE OPCIONES")
+        separador()
         print("\n1. Ver productos")
         print("2. Buscar producto")
         print("3. Salir")
@@ -39,4 +56,5 @@ def menu():
         elif op == "2":
             consultar_producto()
         elif op == "3":
+            limpiar()
             break
--- modeloCascada.md
+++ modeloCascada.md
@@ -5,6 +5,8 @@ Dificultades:
 - Dependencia entre backend y frontend
 - Cambios difíciles de aplicar
 - Errores detectados tarde
+- Dificultad para el avance en paralelo, dependencia y bloqueos entre  backend y frontend
+- Cuando nos piden cambios en el frontend por lo general implica modificaciones del backend y logica
 
 Conclusión:
-El modelo en cascada es rígido y poco flexible.
+El modelo en cascada es rígido y poco flexible, funciona mejor cuando los requisitos son estables y el alcance está muy bien definido desde el inicio. En proyectos con fuerte acoplamiento backend–frontend, suele generar dificultades para avanzar en paralelo y retrabajo. Por eso, es menos adecuado cuando se esperan cambios frecuentes en los requerimientos. 
