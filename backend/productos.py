b16f1f12675185f687cd1a223647ae17da488f0d
juandiego1727 <jdarevalo17@gmail.com>
2026-05-23 09:42:19 -0500

    Update productos.py

--- backend/productos.py
+++ backend/productos.py
@@ -21,3 +21,50 @@ def buscar_producto(productos, id_buscar):
         if p["id"] == id_buscar:
             return p
     return None
+def crear_producto(ruta, id_producto, nombre, precio, cantidad):
+    productos = leer_productos(ruta)
+
+    for p in productos:
+        if p["id"] == id_producto:
+            return False
+
+    archivo = openpyxl.load_workbook(ruta)
+    hoja = archivo.active
+
+    hoja.append([id_producto, nombre, precio, cantidad])
+
+    archivo.save(ruta)
+
+    return True
+    def actualizar_producto(ruta, id_producto, nombre, precio, cantidad):
+    archivo = openpyxl.load_workbook(ruta)
+    hoja = archivo.active
+
+    encontrado = False
+
+    for fila in hoja.iter_rows(min_row=2):
+        if fila[0].value == id_producto:
+            fila[1].value = nombre
+            fila[2].value = precio
+            fila[3].value = cantidad
+            encontrado = True
+            break
+
+    archivo.save(ruta)
+
+    return encontrado
+    def eliminar_producto(ruta, id_producto):
+    archivo = openpyxl.load_workbook(ruta)
+    hoja = archivo.active
+
+    encontrado = False
+
+    for i, fila in enumerate(hoja.iter_rows(min_row=2), start=2):
+        if fila[0].value == id_producto:
+            hoja.delete_rows(i)
+            encontrado = True
+            break
+
+    archivo.save(ruta)
+
+    return encontrado
